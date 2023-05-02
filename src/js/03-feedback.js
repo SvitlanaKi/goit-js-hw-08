eed0955f15d339d62beafee8858dc2f567c6826c
SvitlanaKi <118849159+SvitlanaKi@users.noreply.github.com>
2023-05-02 15:00:44 +0300

    done

---
+++ src/js/03-feedback.js
@@ -0,0 +1,26 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+const formElements = Object.fromEntries(
+  [...form.elements].map(el => [el.name, el])
+);
+let formData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+const saveFormData = throttle(({ target: { name, value } }) => {
+  formData[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
+const handleSubmit = e => {
+  e.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+  formData = {};
+};
+
+form.addEventListener('input', saveFormData);
+form.addEventListener('submit', handleSubmit);
+
+Object.entries(formData).forEach(([name, value]) => {
+  formElements[name].value = value;
+});
